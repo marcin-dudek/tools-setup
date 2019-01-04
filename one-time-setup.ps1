3f167ee14aabcd15a06e511492d7f599ad3e7227
Marcin Dudek <manekd@gmail.com>
2019-01-04 11:17:07 +0100

    add one time setup

---
+++ one-time-setup.ps1
@@ -0,0 +1,3 @@
+# --------- Setup git alias
+git alias.s status -s
+git alias.lg log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
